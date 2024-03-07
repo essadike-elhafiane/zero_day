70636acc69ddcc630e1d207f7c8b4ae6c12bd9ed
essadike elhafiane <eelhafia@student.1337.ma>
2024-03-07 17:25:28 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+const person = {
+  fullName: function() {
+    return this.firstName + " " + this.lastName;
+  }
+}
+
+const person1 = {
+  firstName: "Mary",
+  lastName: "Doe"
+}
+
+// This will return "Mary Doe":
+person.fullName.apply(person1);
